720b7a9df75a2702f3859c2829cab803439da180
kirald17 <kirald17@htl-kaindorf.at>
2021-12-20 16:18:42 +0100

    mehr pos übung + start an bankdb

---
+++ Exa_03_BankDB/src/main/java/at/kaindorf/bank/pojos/KontoRepository.java
@@ -0,0 +1,4 @@
+package at.kaindorf.bank.pojos;
+
+public interface KontoRepository extends org.springframework.data.jpa.repository.JpaRepository<at.kaindorf.bank.pojos.Konto, int> {
+}
---
+++ Exa_03_BankDB/src/main/java/at/kaindorf/bank/pojos/KundeRepository.java
@@ -0,0 +1,4 @@
+package at.kaindorf.bank.pojos;
+
+public interface KundeRepository extends org.springframework.data.jpa.repository.JpaRepository<at.kaindorf.bank.pojos.Kunde, long> {
+}
